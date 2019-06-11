10f162962959198b72a18c6485d3fd9014300183
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-11 16:20:30 +0900

    メソッドがない場合に終了する処理を追加

--- PlayGround/fetch_and_downcase.rb
+++ PlayGround/fetch_and_downcase.rb
@@ -1,8 +1,15 @@
+# frozen_string_literal: true
+
 def fetch_and_downcase(ary, index)
-  if str = ary[index]
-    return str.downcase
-  end
+  str = ary[index]
+  return unless str
+
+  str = str.downcase
+  return unless str
+
+  str
 end
 
-ary = %w[Boo Foo Woo]
+# ary = %w[Boo Foo Woo]
+ary = 5
 p fetch_and_downcase(ary, 1)
