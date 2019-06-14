181eac7d6fdf0e713eaca5d860d4889cf6f73802
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-14 20:34:48 +0900

    問3

---
+++ Exam/Ch17/no3.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+def tail(line_num, filename)
+  raise '行数が負の値です' if line_num.negative?
+
+  text_ary = File.read(filename).split(/\n/)
+  raise '行数がファイルの行数を超えています' if line_num > text_ary.size
+
+  index  = -line_num - 1
+  length = line_num + 1
+  text_ary[index, length]
+end
+
+if $PROGRAM_NAME == __FILE__ && ARGV[0] && ARGV[1]
+  raise "#{ARGV[0]}は数字ではありません" if ARGV[0] != '0' && ARGV[0].to_i.zero?
+
+  line_num = ARGV[0].to_i
+  filename = ARGV[1]
+  begin
+    puts "後ろから#{line_num}行目以降の行を表示します：\n"
+    tail(line_num, filename).each { |line| puts line }
+  rescue Errno::ENOENT, Errno::EACCES
+    puts "ファイル名[#{filename}]を開けません"
+  end
+end
