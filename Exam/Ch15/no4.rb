8008c8ee8a3d15412efa5fbbadd58a0c75943a64
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-14 14:33:02 +0900

    ハッシュの生成方法を変更

--- Exam/Ch15/no4.rb
+++ Exam/Ch15/no4.rb
@@ -2,12 +2,9 @@
 
 # exam no.4
 def str2hash(str)
-  result = {}
-  str.split(/\s+/).each_slice(2) do |a|
-    a[1] ||= ''
-    result[a[0]] = a[1]
-  end
-  result
+  keys_and_val = str.split(/\s+/)
+  keys_and_val << '' if keys_and_val.size.odd?
+  Hash[*keys_and_val]
 end
 
-p str2hash("blue 青 white 白\nred 赤 hoge")
+p str2hash("blue 青 white\t白\nred 赤 hoge ")
