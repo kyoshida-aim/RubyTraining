a3bd31dccaacbe930dbc74d487a962f87242e03c
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-14 11:06:04 +0900

    問1~3まで

---
+++ Exam/Ch15/no1to3.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# exam no.1
+wday =
+  { sunday: '日曜日',
+    monday: '月曜日',
+    tuesday: '火曜日',
+    wednesday: '水曜日',
+    thursday: '木曜日',
+    friday: '金曜日',
+    saturday: '土曜日' }
+
+puts '問1'
+p wday[:sunday]
+p wday[:monday]
+p wday[:saturday]
+puts ''
+
+# exam no.2
+puts '問2'
+puts "ハッシュのサイズ:#{wday.size}"
+puts ''
+
+# exam no.3
+wday.each { |k, v| puts"「#{k}」は#{v}のことです"}
