cef6d3b1346410e453e205c7edc50404c3345acf
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-15 15:23:15 +0900

    問題のとおりに回答を表示

--- Exam/Ch16/no1.rb
+++ Exam/Ch16/no1.rb
@@ -5,6 +5,4 @@ pattern = /(.+)(?:@)(.+)/
 pattern =~ address
 
 puts "メールアドレス: #{address}"
-# same as => p "$1: #{$1}, $2: #{$2}"
-# rubocop required not to use $1
-puts "$1: #{Regexp.last_match(1)}, $2: #{Regexp.last_match(2)}"
+puts "$1: #{$1}, $2: #{$2}" # rubocop:disable Style/PerlBackrefs
