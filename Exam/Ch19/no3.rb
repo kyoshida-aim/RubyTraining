2cc027507df6a040a7e5ac50898d2cde90a23f73
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-17 17:55:57 +0900

    失敗した時にrescueするように

--- Exam/Ch19/no3.rb
+++ Exam/Ch19/no3.rb
@@ -4,4 +4,8 @@ str = '①'
 
 puts "元の文字:#{str.encode('UTF-8')}"
 puts "Windows-31J:#{str.encode('Windows-31J')}"
-puts "Shift_JIS:#{str.encode('Shift_JIS')}"
+begin
+  str.encode('Shift_JIS')
+rescue Encoding::UndefinedConversionError
+  puts "Shift_JISに変換しようとして失敗しました"
+end
