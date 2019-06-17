a1e567eaf362f60abb49ccbc236e4df5aadb7293
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-17 18:00:58 +0900

    文字列のクォーテーションを修正

--- Exam/Ch19/no3.rb
+++ Exam/Ch19/no3.rb
@@ -7,5 +7,5 @@ puts "Windows-31J:#{str.encode('Windows-31J')}"
 begin
   str.encode('Shift_JIS')
 rescue Encoding::UndefinedConversionError
-  puts "Shift_JISに変換しようとして失敗しました"
+  puts 'Shift_JISに変換しようとして失敗しました'
 end
