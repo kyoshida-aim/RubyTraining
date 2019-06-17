8366977f0332cbf2fee54e552435312cc0ddccc2
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-17 10:57:43 +0900

    basenameの取得の仕方を変更
    拡張子を取り除く引数があることを知ったので変更

--- Exam/Ch17/create_path.rb
+++ Exam/Ch17/create_path.rb
@@ -2,6 +2,6 @@
 
 def create_path(old_filename, additional_text)
   extname = File.extname(old_filename)
-  basename = File.basename(old_filename).sub(extname, '')
+  basename = File.basename(old_filename, extname)
   basename + additional_text + extname
 end
