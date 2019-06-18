26c35b28393916f78d461012c98253b0685145dd
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-18 10:14:02 +0900

    問2:出力内容の整理

--- Exam/Ch19/no2.rb
+++ Exam/Ch19/no2.rb
@@ -6,5 +6,13 @@ if $PROGRAM_NAME == __FILE__
   File.open(filename, 'w:Shift_JIS') do |file|
     file.write(text)
   end
+  puts 'Shift_JISでの表示：'
+  file = File.open(filename, 'r:Shift_JIS')
+  p file.read.encode('UTF-8')
+  file.close
+  puts 'コマンド「p」での表示：'
   p File.read(filename).encode('UTF-8')
+  puts 'コマンド「puts」での表示：'
+  puts File.read(filename).encode('UTF-8')
+  File.delete(filename)
 end
