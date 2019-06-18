7601114dce5911ad5de5bc9c61b20b743f248622
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-18 17:05:04 +0900

    問3:コマンドをオブジェクト化

--- Exam/Ch20/no3.rb
+++ Exam/Ch20/no3.rb
@@ -2,30 +2,28 @@
 
 # this class reproduces Unix Command
 class UnixIsh
-  class << self
-    def ls_t(path)
-      return unless File.directory?(path)
+  def ls_t(path)
+    return unless File.directory?(path)
 
-      result = ['最終更新日 : ファイル名']
-      Dir.open(path) do |dir|
-        files_with_ctime(dir) { |name| result << ls_t_format(path, name) }
-      end
-      puts result
+    result = ['最終更新日 : ファイル名']
+    Dir.open(path) do |dir|
+      files_with_ctime(dir) { |name| result << ls_t_format(path, name) }
     end
+    puts result
+  end
 
-    def files_with_ctime(dir)
-      dir.sort.each do |name|
-        next if name == '.'
-        next if name == '..'
+  def files_with_ctime(dir)
+    dir.sort.each do |name|
+      next if name == '.'
+      next if name == '..'
 
-        yield(name)
-      end
+      yield(name)
     end
+  end
 
-    def ls_t_format(path, name)
-      "#{File.stat(path + '/' + name).ctime} : #{name}"
-    end
+  def ls_t_format(path, name)
+    "#{File.stat(path + '/' + name).ctime} : #{name}"
   end
 end
 
-UnixIsh.ls_t(ARGV[0]) if ($PROGRAM_NAME == __FILE__) && ARGV[0]
+UnixIsh.new.ls_t(ARGV[0]) if ($PROGRAM_NAME == __FILE__) && ARGV[0]
