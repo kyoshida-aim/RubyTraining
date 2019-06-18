e860b13fbb52cbb3f39d10e73c1422b0519da082
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-18 14:44:24 +0900

    問1:変数名をシンプルに

--- Exam/Ch20/no1.rb
+++ Exam/Ch20/no1.rb
@@ -3,10 +3,10 @@
 require 'date'
 
 today = Date.today
-ruby_birthday = Date.new(1993, 2, 24)
-ruby_age = today.year - ruby_birthday.year
-ruby_age -= 1 if today.yday < ruby_birthday.yday
+birthday = Date.new(1993, 2, 24)
+age = today.year - birthday.year
+age -= 1 if today.yday < birthday.yday
 
 puts "今日の日付: #{today}"
-puts "rubyの誕生日: #{ruby_birthday}"
-puts "rubyの年齢: #{ruby_age}"
+puts "rubyの誕生日: #{birthday}"
+puts "rubyの年齢: #{age}"
