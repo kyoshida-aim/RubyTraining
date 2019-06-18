0143120fe5f9dc9af6d2cf6e17386b0683b9695a
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-18 14:44:42 +0900

    問1:ydayについてのコメントを追加

--- Exam/Ch20/no1.rb
+++ Exam/Ch20/no1.rb
@@ -5,6 +5,7 @@ require 'date'
 today = Date.today
 birthday = Date.new(1993, 2, 24)
 age = today.year - birthday.year
+# yday returns date as 1-366(Integer)
 age -= 1 if today.yday < birthday.yday
 
 puts "今日の日付: #{today}"
