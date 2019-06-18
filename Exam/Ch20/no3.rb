022fcca82c78aff133f85a456e20621808226a44
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-18 16:01:14 +0900

    問3:ファイルリストの取得方法を変更

--- Exam/Ch20/no3.rb
+++ Exam/Ch20/no3.rb
@@ -5,25 +5,45 @@ class UnixIsh
   def ls_t(path)
     return unless File.directory?(path)
 
-    result = ['最終更新日 : ファイル名']
-    Dir.open(path) do |dir|
-      files_with_ctime(dir) { |name| result << ls_t_format(path, name) }
-    end
-    puts result
-  end
-
-  def files_with_ctime(dir)
-    dir.sort.each do |name|
-      next if name == '.'
-      next if name == '..'
-
-      yield(name)
+    puts '最終更新日 : ファイル名'
+    ls_list = LsList.new(path)
+    ls_list.sorted_by_ctime.each do |ctime, name|
+      puts ls_t_format(ctime, name)
     end
   end
 
-  def ls_t_format(path, name)
-    "#{File.stat(path + '/' + name).ctime} : #{name}"
+  def ls_t_format(ctime, name)
+    "#{ctime} : #{name}"
   end
+end
+
+# class to create list object using at class::UnixIsh
+class LsList
+  def initialize(path)
+    @path = path
+    @list = list
+  end
+
+  def sorted_by_ctime
+    # item[0] is updated time of file
+    @list.sort_by { |item| item[0] }
+  end
+
+  private
+
+  def list
+    list = []
+    Dir.open(@path) do |dir|
+      dir.each do |name|
+        next if name == '.'
+        next if name == '..'
+
+        list << [File.stat(@path + '/' + name).ctime, @path + '/' + name]
+      end
+    end
+    list
+  end
+
 end
 
 UnixIsh.new.ls_t(ARGV[0]) if ($PROGRAM_NAME == __FILE__) && ARGV[0]
