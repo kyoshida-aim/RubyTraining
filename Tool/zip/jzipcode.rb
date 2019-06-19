0309f7fc26a384771ca10bef45301132f66b7862
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-19 18:28:45 +0900

    SQLコマンドを独立

--- Tool/zip/jzipcode.rb
+++ Tool/zip/jzipcode.rb
@@ -3,6 +3,41 @@
 require 'sqlite3'
 require 'csv'
 
+# class to call SQL commands
+class SQLCommands
+  class << self
+    def create_table
+      <<-SQL
+        CREATE TABLE IF NOT EXISTS zip_codes
+        (code TEXT, pref TEXT, city TEXT, addr TEXT, alladdr TEXT)
+      SQL
+    end
+
+    def write_line
+      <<-SQL
+          INSERT INTO zip_codes VALUES
+            (:code, :pref, :city, :addr, :alladdr)
+      SQL
+    end
+
+    def find_by_code
+      <<-SQL
+        SELECT code, alladdr
+          FROM zip_codes
+          WHERE code = ?
+      SQL
+    end
+
+    def find_by_address
+      <<-SQL
+        SELECT code, alladdr
+          FROM zip_codes
+          WHERE alladdr LIKE ?
+      SQL
+    end
+  end
+end
+
 # this class will create database of Zip and enable searching.
 class JZipCode
   CSV_COLUMN = { code: 2, pref: 6, city: 7, addr: 8 }.freeze
@@ -15,20 +50,15 @@ class JZipCode
     return if File.exist?(@dbfile)
 
     SQLite3::Database.open(@dbfile) do |db|
-      db.execute(<<-SQL)
-        CREATE TABLE IF NOT EXISTS zip_codes
-        (code TEXT, pref TEXT, city TEXT, addr TEXT, alladdr TEXT)
-      SQL
+      db.execute(SQLCommands.create_table)
+
       db.execute('BEGIN TRANSACTION')
       CSV.open(zipfile, 'r') do |csv|
         csv.each do |rec|
           data = {}
           CSV_COLUMN.each { |key, index| data[key] = rec[index] }
           data[:alladdr] = data[:pref] + data[:city] + data[:addr]
-          db.execute(<<-SQL, data)
-            INSERT INTO zip_codes VALUES
-              (:code, :pref, :city, :addr, :alladdr)
-          SQL
+          db.execute(SQLCommands.write_line, data)
         end
       end
       db.execute('COMMIT TRANSACTION')
@@ -39,11 +69,7 @@ class JZipCode
   def find_by_code(code)
     ret = []
     SQLite3::Database.open(@dbfile) do |db|
-      db.execute(<<-SQL, code) { |row| ret << row.join(' ') }
-        SELECT code, alladdr
-          FROM zip_codes
-          WHERE code = ?
-      SQL
+      db.execute(SQLCommands.find_by_code, code) { |row| ret << row.join(' ') }
     end
     ret.map { |line| line + '\n' }.join
   end
@@ -52,11 +78,7 @@ class JZipCode
     ret = []
     SQLite3::Database.open(@dbfile) do |db|
       like = "%#{addr}%"
-      db.execute(<<-SQL, like) { |row| ret << row.join(' ') }
-        SELECT code, alladdr
-          FROM zip_codes
-          WHERE alladdr LIKE ?
-      SQL
+      db.execute(SQLCommands.find_by_address, like) { |row| ret << row.join(' ') }
     end
     ret.map { |line| line + '\n' }.join
   end
