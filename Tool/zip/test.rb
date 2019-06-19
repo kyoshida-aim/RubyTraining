bf56604cfc1df0c7d5eb871b5c899f419637cd38
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-19 18:15:34 +0900

    テストケースの出力詳細

--- Tool/zip/test.rb
+++ Tool/zip/test.rb
@@ -4,5 +4,8 @@ require './jzipcode_original'
 jzipcode = JZipCode.new('jzipcode.db')
 jzipcode.create('KEN_ALL.CSV')
 
+puts '351-0025での検索結果:'
 puts jzipcode.find_by_code('3510025')
+
+puts '埼玉県朝霞市での検索結果:'
 puts jzipcode.find_by_address('埼玉県朝霞市')
