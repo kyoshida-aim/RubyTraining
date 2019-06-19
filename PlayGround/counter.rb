14f62bb74e68ee538806801460dde28b1382dcf6
KyoheiYoshida <kyoshida@aiming-inc.com>
2019-06-19 10:30:06 +0900

    ローカル変数の扱いを確認できるスクリプト

---
+++ PlayGround/counter.rb
@@ -0,0 +1,26 @@
+# frozen_string_litreal: true
+
+def counter
+  c = 0
+  proc do
+    c += 1
+  end
+end
+
+# create counter `c1` and count up
+
+c1 = counter
+
+puts c1.call #=>1
+puts c1.call #=>2
+puts c1.call #=>3
+
+# create counter `c2` and count up
+c2 = counter #
+
+puts c2.call #=>1
+puts c2.call #=>2
+
+# c1 and c2 have different local variables
+
+puts c1.call #=>4
